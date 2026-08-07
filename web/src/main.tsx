10aa4aac6c00727ffb42ffe38b92b09517bc1ce7
PendoWolf <pendo+githubnovus@qawolf.email>
2026-08-07 16:27:37 +0000

    Initial commit

---
+++ web/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+let visitorId = localStorage.getItem('demo-visitor-id');
+if (!visitorId) {
+  visitorId = 'visitor-' + Math.random().toString(36).slice(2);
+  localStorage.setItem('demo-visitor-id', visitorId);
+}
+pendo.initialize({
+  visitor: {
+    id: visitorId,
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
